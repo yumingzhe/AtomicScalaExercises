c5838ab6e919e770c8ff74f95576d715c088ebce
yumingzhe <yumingzhe@live.cn>
2017-10-03 13:43:19 +0800

    添加数据类型练习

---
+++ p26.scala
@@ -0,0 +1,33 @@
+// 1.
+val value1: Int = 5
+println(value1)
+
+// 2.
+val str: String = "ABC1234"
+println(str)
+
+// 3.
+val doubleValue: Double = 5.4
+println(doubleValue)
+
+// 4.
+val booleanValue = true
+println(booleanValue)
+
+// 5.
+val longStr = """this is
+a
+ver long string"""
+println(longStr)
+
+// 6.
+// val booleanValue2: Boolean = "maybe"
+// error: type mismatch;
+
+// 7.
+// val intValue: Int = 15.4
+// println(intValue) //error: type mismatch;
+
+// 8.
+val doubleValue2:Double = 15
+println(doubleValue2)
