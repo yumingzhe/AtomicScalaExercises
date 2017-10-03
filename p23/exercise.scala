68e2bf36f2b14138801e1ed99c75ce5a8da6ae33
yumingzhe <yumingzhe@live.cn>
2017-10-03 12:49:21 +0800

    add first exercise

---
+++ p23/exercise.scala
@@ -0,0 +1,17 @@
+// 1
+val value1 = 17
+println(value1)
+
+// 2
+// value1 = 18 //error: reassignment to val
+
+//3
+val str = "ABC1234"
+println(str)
+
+// 4
+//str = "DEF1234" //error: reassignment to val
+
+// 5
+val number = 15.56
+println(number)
