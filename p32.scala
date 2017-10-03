59467a3ee13d2f8c145d50e7077dffe6b6207d64
yumingzhe <yumingzhe@live.cn>
2017-10-03 14:58:44 +0800

    增加条件表达式练习

---
+++ p32.scala
@@ -0,0 +1,34 @@
+// 1.
+val a: Int = 1
+val b: Int = 5
+if(a < b){
+    println("a is less than b")
+} else {
+    println("b is less than a")
+}
+
+// 2.
+if(a > 2){
+    println("a > 2")
+}else{
+    println("a < 2")
+}
+
+if(b > 2){
+    println("b > 2")
+}else{
+    println("b < 2")
+}
+
+// 3.
+val c: Int = 5
+if(a < c){
+    println("a < c")
+}else{
+    println("a >= c")
+}
+if(b < c){
+    println("b < c")
+}else{
+    println("b >= c")
+}
