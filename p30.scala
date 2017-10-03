efe923fe6c686f983ede6ebb344fb8c7c56dcac5
yumingzhe <yumingzhe@live.cn>
2017-10-03 14:38:49 +0800

    增加表达式练习

---
+++ p30.scala
@@ -0,0 +1,10 @@
+// 1.
+var feetPerMile = 5280
+// 2.
+var yardsPerMile = feetPerMile / 3.0
+// 3.
+var miles = 2000 / yardsPerMile
+println(miles) // 1.1363636363636365
+// 4.
+var miles2 = 2000 / (5280/3.0)
+println(miles) // 1.1363636363636365
