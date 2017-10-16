ee0d6534e201f1632cfb024d5b20ae3436f804cf
yumingzhe <yumingzhe@live.cn>
2017-10-16 13:43:53 +0800

    增加练习第3题

--- p105.scala
+++ p105.scala
@@ -15,7 +15,7 @@ tripleHalfCaf.caf is 1
 tripleHalfCaf.shots is 3
 
 // 2.
-class Tea(val name: String = "Earl Grey", decaf: Boolean = false, milk: Boolean = false, sugar: Boolean = false) {
+class Tea(name: String = "Earl Grey", decaf: Boolean = false, milk: Boolean = false, sugar: Boolean = false) {
   def describe(): String = {
     var s: String = name
     if (decaf) {
@@ -54,3 +54,45 @@ sweetGreen.calories() is 16
 val teaLatte = new Tea(sugar = true, milk = true)
 teaLatte.describe() is "Earl Grey + milk + sugar"
 teaLatte.calories() is 116
+
+// 3.
+class Tea2(val name: String = "Earl Grey", val decaf: Boolean = false, val milk: Boolean = false, val sugar: Boolean = false) {
+  var describe: String = name
+  if (decaf) {
+    describe += " decaf"
+  }
+  if (milk) {
+    describe += " + milk"
+  }
+  if (sugar) {
+    describe += " + sugar"
+  }
+
+  var calories = 0
+  if (milk) {
+    calories += 100
+  }
+  if (sugar) {
+    calories += 16
+  }
+}
+
+val tea2 = new Tea2
+tea2.describe is "Earl Grey"
+tea2.calories is 0
+tea2.name is "Earl Grey"
+
+val lemonZinger2 = new Tea2(decaf = true, name = "Lemon Zinger")
+lemonZinger2.describe is "Lemon Zinger decaf"
+lemonZinger2.calories is 0
+lemonZinger2.decaf is true
+
+val sweetGreen2 = new Tea2(name = "Jasmine Green", sugar = true)
+sweetGreen2.describe is "Jasmine Green + sugar"
+sweetGreen2.calories is 16
+sweetGreen2.sugar is true
+
+val teaLatte2 = new Tea2(sugar = true, milk = true)
+teaLatte2.describe is "Earl Grey + milk + sugar"
+teaLatte2.calories is 116
+teaLatte2.milk is true
